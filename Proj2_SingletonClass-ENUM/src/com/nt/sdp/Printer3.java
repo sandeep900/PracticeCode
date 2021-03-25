4ab84cb6c4dea54254690496b3c3db576d54f101
TL <TL@gmail.com>
2021-03-25 14:08:25 +0530

    initial 2nd project

---
+++ Proj2_SingletonClass-ENUM/src/com/nt/sdp/Printer3.java
@@ -0,0 +1,15 @@
+package com.nt.sdp;
+
+public enum Printer3 {
+ instance;//public static final Printer3 instance= new Printer3()
+	        //eager instantion
+	
+	public static Printer3 getinstance() {
+		
+		return instance;
+		
+	}
+	
+	
+
+}
